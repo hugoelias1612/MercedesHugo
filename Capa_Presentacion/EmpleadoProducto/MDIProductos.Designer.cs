fd6e6610a5337130cedd158f45c041850afac502
MercedesFernandez <mercedessilvanafernandez37@gmail.com>
2025-09-01 09:18:51 -0300

    resolucion merge

--- Capa_Presentacion/EmpleadoProducto/MDIProductos.Designer.cs
+++ Capa_Presentacion/EmpleadoProducto/MDIProductos.Designer.cs
@@ -42,15 +42,9 @@ namespace ArimaERP.EmpleadoProducto
             this.BStock = new System.Windows.Forms.Button();
             this.BABM = new System.Windows.Forms.Button();
             this.BAlerta = new System.Windows.Forms.Button();
-<<<<<<< HEAD
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-=======
             this.PContent = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.TLPBody.SuspendLayout();
             this.TLPBotones.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
@@ -102,15 +96,9 @@ namespace ArimaERP.EmpleadoProducto
             this.TLPBotones.Controls.Add(this.BStock, 2, 0);
             this.TLPBotones.Controls.Add(this.BABM, 1, 0);
             this.TLPBotones.Controls.Add(this.BAlerta, 0, 0);
-<<<<<<< HEAD
-            this.TLPBotones.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TLPBotones.Location = new System.Drawing.Point(0, 12);
-            this.TLPBotones.Margin = new System.Windows.Forms.Padding(0, 12, 0, 0);
-=======
             this.TLPBotones.Dock = System.Windows.Forms.DockStyle.Top;
             this.TLPBotones.Location = new System.Drawing.Point(0, 10);
             this.TLPBotones.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.TLPBotones.Name = "TLPBotones";
             this.TLPBotones.RowCount = 1;
             this.TLPBotones.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
@@ -124,17 +112,10 @@ namespace ArimaERP.EmpleadoProducto
             this.BPerfil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.BPerfil.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BPerfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-<<<<<<< HEAD
-            this.BPerfil.Location = new System.Drawing.Point(1402, 3);
-            this.BPerfil.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BPerfil.Name = "BPerfil";
-            this.BPerfil.Size = new System.Drawing.Size(133, 91);
-=======
             this.BPerfil.Location = new System.Drawing.Point(1052, 3);
             this.BPerfil.Margin = new System.Windows.Forms.Padding(2);
             this.BPerfil.Name = "BPerfil";
             this.BPerfil.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BPerfil.TabIndex = 20;
             this.BPerfil.UseVisualStyleBackColor = false;
             // 
@@ -146,16 +127,9 @@ namespace ArimaERP.EmpleadoProducto
             this.BSalir.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BSalir.Location = new System.Drawing.Point(1543, 5);
-            this.BSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.BSalir.Name = "BSalir";
-            this.BSalir.Size = new System.Drawing.Size(137, 87);
-=======
             this.BSalir.Location = new System.Drawing.Point(1158, 4);
             this.BSalir.Name = "BSalir";
             this.BSalir.Size = new System.Drawing.Size(102, 71);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BSalir.TabIndex = 16;
             this.BSalir.UseVisualStyleBackColor = false;
             // 
@@ -167,17 +141,10 @@ namespace ArimaERP.EmpleadoProducto
             this.BComprar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BComprar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BComprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BComprar.Location = new System.Drawing.Point(704, 3);
-            this.BComprar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BComprar.Name = "BComprar";
-            this.BComprar.Size = new System.Drawing.Size(133, 91);
-=======
             this.BComprar.Location = new System.Drawing.Point(528, 3);
             this.BComprar.Margin = new System.Windows.Forms.Padding(2);
             this.BComprar.Name = "BComprar";
             this.BComprar.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BComprar.TabIndex = 8;
             this.BComprar.UseVisualStyleBackColor = false;
             // 
@@ -189,17 +156,10 @@ namespace ArimaERP.EmpleadoProducto
             this.BBuscar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BBuscar.Location = new System.Drawing.Point(564, 3);
-            this.BBuscar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BBuscar.Name = "BBuscar";
-            this.BBuscar.Size = new System.Drawing.Size(133, 91);
-=======
             this.BBuscar.Location = new System.Drawing.Point(423, 3);
             this.BBuscar.Margin = new System.Windows.Forms.Padding(2);
             this.BBuscar.Name = "BBuscar";
             this.BBuscar.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BBuscar.TabIndex = 4;
             this.BBuscar.UseVisualStyleBackColor = false;
             this.BBuscar.Click += new System.EventHandler(this.button4_Click);
@@ -212,17 +172,10 @@ namespace ArimaERP.EmpleadoProducto
             this.BAgregar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BAgregar.Location = new System.Drawing.Point(424, 3);
-            this.BAgregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BAgregar.Name = "BAgregar";
-            this.BAgregar.Size = new System.Drawing.Size(133, 91);
-=======
             this.BAgregar.Location = new System.Drawing.Point(318, 3);
             this.BAgregar.Margin = new System.Windows.Forms.Padding(2);
             this.BAgregar.Name = "BAgregar";
             this.BAgregar.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BAgregar.TabIndex = 3;
             this.BAgregar.UseVisualStyleBackColor = false;
             this.BAgregar.Click += new System.EventHandler(this.BAgregar_Click);
@@ -235,17 +188,10 @@ namespace ArimaERP.EmpleadoProducto
             this.BStock.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BStock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BStock.Location = new System.Drawing.Point(284, 3);
-            this.BStock.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BStock.Name = "BStock";
-            this.BStock.Size = new System.Drawing.Size(133, 91);
-=======
             this.BStock.Location = new System.Drawing.Point(213, 3);
             this.BStock.Margin = new System.Windows.Forms.Padding(2);
             this.BStock.Name = "BStock";
             this.BStock.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BStock.TabIndex = 2;
             this.BStock.UseVisualStyleBackColor = false;
             this.BStock.Click += new System.EventHandler(this.BStock_Click);
@@ -258,17 +204,10 @@ namespace ArimaERP.EmpleadoProducto
             this.BABM.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BABM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BABM.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BABM.Location = new System.Drawing.Point(144, 3);
-            this.BABM.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BABM.Name = "BABM";
-            this.BABM.Size = new System.Drawing.Size(133, 91);
-=======
             this.BABM.Location = new System.Drawing.Point(108, 3);
             this.BABM.Margin = new System.Windows.Forms.Padding(2);
             this.BABM.Name = "BABM";
             this.BABM.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BABM.TabIndex = 1;
             this.BABM.UseVisualStyleBackColor = false;
             // 
@@ -280,33 +219,14 @@ namespace ArimaERP.EmpleadoProducto
             this.BAlerta.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BAlerta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BAlerta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.BAlerta.Location = new System.Drawing.Point(4, 3);
-            this.BAlerta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BAlerta.Name = "BAlerta";
-            this.BAlerta.Size = new System.Drawing.Size(133, 91);
-=======
             this.BAlerta.Location = new System.Drawing.Point(3, 3);
             this.BAlerta.Margin = new System.Windows.Forms.Padding(2);
             this.BAlerta.Name = "BAlerta";
             this.BAlerta.Size = new System.Drawing.Size(100, 73);
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.BAlerta.TabIndex = 0;
             this.BAlerta.Text = "\r\n";
             this.BAlerta.UseVisualStyleBackColor = false;
             this.BAlerta.Click += new System.EventHandler(this.BAlerta_Click);
-            // 
-<<<<<<< HEAD
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 121);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0, 12, 0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1685, 717);
-            this.panel1.TabIndex = 1;
-=======
             // PContent
             // 
             this.PContent.BackColor = System.Drawing.SystemColors.ControlLightLight;
@@ -316,8 +236,6 @@ namespace ArimaERP.EmpleadoProducto
             this.PContent.Name = "PContent";
             this.PContent.Size = new System.Drawing.Size(1264, 582);
             this.PContent.TabIndex = 1;
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
-            // 
             // timer1
             // 
             this.timer1.Enabled = true;
@@ -331,11 +249,6 @@ namespace ArimaERP.EmpleadoProducto
             this.ClientSize = new System.Drawing.Size(1685, 838);
             this.Controls.Add(this.TLPBody);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-<<<<<<< HEAD
-            this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-=======
->>>>>>> ae148a34cce8d430b156c9a1a11bba668fd465d9
             this.MaximizeBox = false;
             this.Name = "MDIProductos";
             this.Text = "Arima ERP - Empleado Productos";
