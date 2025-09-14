e7a15ea9e8e2cd4b0663c4206916e68967cc136e
MercedesFernandez <mercedessilvanafernandez37@gmail.com>
2025-09-14 14:35:15 -0300

    aceptar todos los cambios

--- Capa_Presentacion/Program.cs
+++ Capa_Presentacion/Program.cs
@@ -14,14 +14,8 @@ namespace ArimaERP
            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-
             //Application.Run(new EmpleadoProducto.MDIProductos());
            Application.Run(new EmpleadoClientes.MDIClientes());
-=======
-            Application.Run(new EmpleadoProducto.MDIProductos());
-           //Application.Run(new EmpleadoClientes.MDIClientes());
->>>>>>> 73f7ea7dcd751b002305568bb7a124eb9b8385b8
            // Application.Run(new Administrador.FormRegistrarUsuario());
            //Application.Run(new Administrador.FormAgregarRol());
             //Application.Run(new FormRegistrarUsuario());
